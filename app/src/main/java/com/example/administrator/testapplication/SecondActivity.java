d4f7b5617f2fc0afc9de6b5e5fa75cf2a6e402b9
Allen <773765415@qq.com>
2016-01-05 10:13:10 +0800

    fourth commit

--- app/src/main/java/com/example/administrator/testapplication/SecondActivity.java
+++ app/src/main/java/com/example/administrator/testapplication/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.administrator.testapplication;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 
 public class SecondActivity extends AppCompatActivity{
@@ -10,5 +11,10 @@ public class SecondActivity extends AppCompatActivity{
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_second);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
