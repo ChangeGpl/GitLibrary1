db88eb6255df3dabdda116d5789361771278b361
Allen <773765415@qq.com>
2016-01-05 10:02:05 +0800

    third commit

---
+++ app/src/main/java/com/example/administrator/testapplication/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.testapplication;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class SecondActivity extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_second);
+    }
+}
